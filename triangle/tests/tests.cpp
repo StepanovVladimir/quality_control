4ea6dcf671f2c1cae39fcdd1e5b21e9f2637f105
Vladimir Stepanov <vovan.number.one@mail.ru>
2019-10-02 08:41:41 +0400

    Тесты

--- triangle/tests/tests.cpp
+++ triangle/tests/tests.cpp
@@ -7,9 +7,33 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+	ifstream tests("tests.txt");
+	ofstream testResults("results.txt");
 	string programPath = string(argv[1]);
-	programPath += " 2 2 3";
-	cout << system(programPath.data());
+	string args;
+	
+	int testNumber = 1;
+	while (getline(tests, args))
+	{
+		string command = programPath + ' ' + args;
+		system(command.data());
 
-	ifstream fIn("output.txt");
+		ifstream fIn("output.txt");
+		string result;
+		getline(fIn, result);
+
+		string expectedResult;
+		getline(tests, expectedResult);
+
+		if (result == expectedResult)
+		{
+			testResults << testNumber << " sucсess;\n";
+		}
+		else
+		{
+			testResults << testNumber << " error;\n";
+		}
+
+		testNumber++;
+	}
 }
